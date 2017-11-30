fe19afa4c8a790cdae4091a0ebb991bd7ba11b7b
geo <geo.stanciu@outlook.com>
2017-11-30 18:50:31 +0200

    HTTP + HTTPS get example

---
+++ PocoExamples/PocoExamples/HTTPUtils.cpp
@@ -0,0 +1,84 @@
+#include "stdafx.h"
+#include "HTTPUtils.h"
+#include <fstream>
+
+
+HTTPUtils::HTTPUtils()
+{
+}
+
+
+HTTPUtils::~HTTPUtils()
+{
+}
+
+bool doRequest(HTTPClientSession& session, HTTPRequest& request, HTTPResponse& response, ostream &stream) {
+	session.sendRequest(request);
+	istream& rs = session.receiveResponse(response);
+
+	if (response.getStatus() == HTTPResponse::HTTP_OK)
+	{
+		StreamCopier::copyStream(rs, stream);
+		return true;
+	}
+	else
+	{
+		Poco::NullOutputStream null;
+		StreamCopier::copyStream(rs, null);
+		return false;
+	}
+}
+
+bool doRequest(HTTPSClientSession& session, HTTPRequest& request, HTTPResponse& response, ostream &stream) {
+	session.sendRequest(request);
+	istream& rs = session.receiveResponse(response);
+
+	if (response.getStatus() == HTTPResponse::HTTP_OK)
+	{
+		StreamCopier::copyStream(rs, stream);
+		return true;
+	}
+	else
+	{
+		Poco::NullOutputStream null;
+		StreamCopier::copyStream(rs, null);
+		return false;
+	}
+}
+
+bool HTTPUtils::http_get(string url, HTTPResponse &response, ostream &stream) {
+	try {
+		bool useHTTPS = false;
+		URI uri(url);
+		string path(uri.getPathAndQuery());
+		if (path.empty()) path = "/";
+
+		string scheme(uri.getScheme());
+		if (scheme == "HTTPS" || scheme == "https") useHTTPS = true;
+
+		if (useHTTPS) {
+			const Context::Ptr context = new Context(
+				Context::CLIENT_USE,
+				"",
+				"",
+				"",
+				Context::VERIFY_NONE,
+				9,
+				false, // don't load default CAs
+				"ALL:!ADH:!LOW:!EXP:!MD5:@STRENGTH");
+
+			HTTPSClientSession session(uri.getHost(), uri.getPort(), context);
+			HTTPRequest request(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
+			return doRequest(session, request, response, stream);
+		}
+		else {
+			HTTPClientSession session(uri.getHost(), uri.getPort());
+			HTTPRequest request(HTTPRequest::HTTP_GET, path, HTTPMessage::HTTP_1_1);
+			return doRequest(session, request, response, stream);
+		}
+	}
+	catch (Exception& exc) {
+		cerr << exc.displayText() << endl;
+		return false;
+	}
+}
---
+++ PocoExamples/PocoExamples/HTTPUtils.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "Poco/Net/HTTPClientSession.h"
+#include "Poco/Net/HTTPSClientSession.h"
+#include "Poco/Net/Context.h"
+#include "Poco/Net/HTTPRequest.h"
+#include "Poco/Net/HTTPResponse.h"
+#include <Poco/Net/HTTPCredentials.h>
+#include "Poco/StreamCopier.h"
+#include "Poco/NullStream.h"
+#include "Poco/Path.h"
+#include "Poco/URI.h"
+#include "Poco/Exception.h"
+#include <fstream>
+
+using namespace std;
+using namespace Poco;
+using namespace Poco::Net;
+
+class HTTPUtils
+{
+public:
+	HTTPUtils();
+	~HTTPUtils();
+
+	bool http_get(string url, HTTPResponse &response, ostream &stream);
+};
